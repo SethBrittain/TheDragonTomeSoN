8966a592ee387158c76bd0c4bee1521ea94a56bc
SethBrittain <sethabrittain@gmail.com>
2021-01-05 04:29:03 -0600

    Rune Allocation System
    Added a rune allocation system to adjust player stats and started testing customization system.

--- Scripts/SceneManger.gd
+++ Scripts/SceneManger.gd
@@ -5,3 +5,7 @@ export (PackedScene) var play_scene
 func activate_play_button():
 	var scene = play_scene.instance()
 	add_child(scene)
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_end"):
+		get_tree().quit()
